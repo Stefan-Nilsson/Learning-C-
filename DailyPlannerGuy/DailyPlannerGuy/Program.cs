490d709fe90826326cff5009dc76671ca3e4d1a2
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-05-07 12:51:23 +0200

    add intro

---
+++ DailyPlannerGuy/DailyPlannerGuy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DailyPlannerGuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello I'm DailyPlannerGuy but you can call me DailyPlannerGuy.");
+            Console.WriteLine("Everyday when starting your day you can talk to me to put some structure in your feelings, thoughts, and plans for the day.");
+            Console.WriteLine("Anyhow, enough about me, lets talk about you.");
+            Console.WriteLine("How are you feeling today?");
+        }
+    }
+}
