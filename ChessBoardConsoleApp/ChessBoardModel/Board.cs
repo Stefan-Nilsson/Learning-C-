d2f3773a125b72bb5cc21167f290abc42d10a38b
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-05-08 12:05:34 +0200

    add Knight moves

--- ChessBoardConsoleApp/ChessBoardModel/Board.cs
+++ ChessBoardConsoleApp/ChessBoardModel/Board.cs
@@ -7,14 +7,19 @@ namespace ChessBoardModel
     class Board
     {
         // the size of the board usually 8x8
-        // initial size of the board is defined by s.
+
         public int Size { get; set; }
-        // create a new 2D array of type cell.
+
+        // 2D array of type cell.
         public Cell[,] theGrid { get; set; }
+
+        // constructor
         public Board(int s)
         {
+            // initial size of the board is defined by s.
             Size = s;
 
+            // create a new 2D array of type cell.
             theGrid = new Cell[Size, Size];
             // fill the 2D array with new Cells, each unique x and y coordinates
             for (int i = 0; i < Size; i++)
@@ -22,6 +27,61 @@ namespace ChessBoardModel
                 for (int j = 0; j < Size; j++)
                 {
                     theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+        public void MarkNextLegalMoves(Cell currentCell, string chessPiece)
+        {
+            // step 1 - clear all previous legal moves
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+
+                // step 2 - find all legal moves and mark the cells as "legal"
+                switch (chessPiece)
+                {
+                    case "Knight":
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+
+
+
+
+
+
+
+                        break;
+
+                    case "King":
+                        break;
+
+                    case
+                        "Rook":
+                        break;
+
+                    case
+                        "Bishop":
+                        break;
+
+                    case
+                        "Queen":
+                        break;
+
+                    case
+                        "Pawn":
+                        break;
+
+
                 }
             }
         }
