4f7ee532ae42d673c8cc6c1e4e4c1a277e73feaa
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-05-20 15:10:23 +0200

    start new project

---
+++ Rock, paper, scissors/Rock, paper, scissors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rock__paper__scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
