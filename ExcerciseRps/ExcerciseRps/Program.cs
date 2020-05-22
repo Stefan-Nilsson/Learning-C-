5d32e6c0c280da874a611cb5e90043e1f58f83a5
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-05-22 12:27:40 +0200

    finishing touches, score count added

--- ExcerciseRps/ExcerciseRps/Program.cs
+++ ExcerciseRps/ExcerciseRps/Program.cs
@@ -7,9 +7,12 @@ namespace ExcerciseRps
         static bool again = true;
         static void Main(string[] args)
         {
+            int userScore = 0;
+            int computerScore = 0;
             while (again)
             {
                 Console.Clear();
+                Console.WriteLine("userscore :{0} computerscore:{1}", userScore, computerScore);
                 Console.WriteLine("Pick rock, paper or scizzors!");
                 string userInput;
                 Random rnd = new Random();
@@ -25,19 +28,61 @@ namespace ExcerciseRps
                         {
                             Console.WriteLine("it's a tie");
                         }
+                        else if (userInput == "paper")
+                        {
+                            Console.WriteLine("you have won this round");
+                            userScore++;
+                        }
+                        else if (userInput == "scizzors")
+                        {
+                            Console.WriteLine("you have lost this round");
+                            computerScore++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("input was not valid");
+                        }                            
                         break;
                     case 2:
                         Console.WriteLine("PAPER");
-                        if (userInput == "paper")
+                        if (userInput == "rock")
+                        {
+                            Console.WriteLine("you have lost this round");
+                            computerScore++;
+                        }
+                        else if (userInput == "paper")
                         {
                             Console.WriteLine("it's a tie");
+                        }
+                        else if (userInput == "scizzors")
+                        {
+                            Console.WriteLine("you have won this round");
+                            userScore++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("input was not valid");
                         }
                         break;
                     default:
                         Console.WriteLine("SCIZZORS");
-                        if (userInput == "scizzors")
+                        if (userInput == "rock")
+                        {
+                            Console.WriteLine("you have won this round");
+                            userScore++;
+                        }
+                        else if (userInput == "paper")
+                        {
+                            Console.WriteLine("you have lost this round");
+                            computerScore++;
+                        }
+                        else if (userInput == "scizzors")
                         {
                             Console.WriteLine("it's a tie");
+                        }
+                        else
+                        {
+                            Console.WriteLine("input was not valid");
                         }
                         break;
                 }
