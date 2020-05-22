4cf653e4d4291bdf5d0c7c11ed01b2e7fee3247c
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-05-22 10:13:02 +0200

    add practice

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                int a = 3;
+                Console.WriteLine(a *= a += a);
+            }
+        }
+    }
+}
